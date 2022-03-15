ea220cef79d05c751c4a2279f4e018600b82fcb9
Sebastian Andres Gallardo <sebastian.gallardo1@mail.udp.cl>
2022-03-15 00:05:44 -0300

    add routes and components/login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
   ],
   imports: [
     ReactiveFormsModule,
     BrowserModule,
     SharedModule,
+    AppRoutingModule,
     
   ],
   providers: [],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDogComponent } from './form-dog/form-dog.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 
 @NgModule({
   declarations: [
-    FormDogComponent
+    FormDogComponent,
+    FormLoginComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  exports: [FormDogComponent]
+  exports: [FormDogComponent, FormLoginComponent]
 })
 export class SharedModule { }
