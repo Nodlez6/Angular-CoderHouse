650f707f9766cbcd25f55a19ae379142269b83ad
Sebastian Andres Gallardo <sebastian.gallardo1@mail.udp.cl>
2022-03-16 11:48:18 -0300

    add navbar and logo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,22 @@ import { FormDogComponent } from './form-dog/form-dog.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormLoginComponent } from './form-login/form-login.component';
 import { FormRegisterComponent } from './form-register/form-register.component';
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     FormDogComponent,
     FormLoginComponent,
-    FormRegisterComponent
+    FormRegisterComponent,
+    NavBarComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forChild([])
   ],
-  exports: [FormDogComponent, FormLoginComponent, FormRegisterComponent]
+  exports: [FormDogComponent, FormLoginComponent, FormRegisterComponent, NavBarComponent]
 })
 export class SharedModule { }
